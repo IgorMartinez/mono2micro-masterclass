31ff9b57e4e287cb5d0c7bbfc2aa82ae83991a84
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-13 16:45:02 -0300

    feat(travel-order): Return more information when use findAll

--- mono/src/main/java/br/com/igormartinez/travelorder/TravelOrderResource.java
+++ mono/src/main/java/br/com/igormartinez/travelorder/TravelOrderResource.java
@@ -2,6 +2,10 @@ package br.com.igormartinez.travelorder;
 
 import java.util.List;
 
+import br.com.igormartinez.flight.Flight;
+import br.com.igormartinez.flight.FlightResource;
+import br.com.igormartinez.hotel.Hotel;
+import br.com.igormartinez.hotel.HotelResource;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -13,11 +17,28 @@ import jakarta.ws.rs.core.MediaType;
 
 @Path("travel-orders")
 public class TravelOrderResource {
-    
+
+    private FlightResource flightResource;
+    private HotelResource hotelResource;
+
+    public TravelOrderResource(FlightResource flightResource, HotelResource hotelResource) {
+        this.flightResource = flightResource;
+        this.hotelResource = hotelResource;
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<TravelOrder> findAll() {
-        return TravelOrder.listAll();
+    public List<TravelOrderDTO> findAll() {
+        return TravelOrder.<TravelOrder>listAll()
+            .stream()
+            .map(
+                order -> TravelOrderDTO.of(
+                    order, 
+                    flightResource.findByTravelOrderId(order.id), 
+                    hotelResource.findByTravelOrderId(order.id)
+                )
+            )
+            .toList();
     }
 
     @GET
@@ -31,10 +52,21 @@ public class TravelOrderResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Transactional
-    public TravelOrder create(TravelOrder order) {
-        order.id = null;
+    public TravelOrder create(TravelOrderDTO orderDTO) {
+        TravelOrder order = new TravelOrder();
         order.persist();
+
+        Flight flight = new Flight();
+        flight.travelOrderId = order.id;
+        flight.fromAirport = orderDTO.getFromAirport();
+        flight.toAirport = orderDTO.getToAirport();
+        flightResource.create(flight);
         
+        Hotel hotel = new Hotel();
+        hotel.travelOrderId = order.id;
+        hotel.nights = orderDTO.getNights();
+        hotelResource.create(hotel);
+
         return order; 
     }
 }
--- mono/src/main/resources/import.sql
+++ mono/src/main/resources/import.sql
@@ -1,21 +1,6 @@
 CREATE SEQUENCE travelorder_sequence START WITH 1 INCREMENT BY 1;
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
 
 CREATE SEQUENCE flight_sequence START WITH 1 INCREMENT BY 1;
-INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),1,'GRU','MCO');
-INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),2,'GRU','JFK');
-INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),3,'GRU','ATL');
-INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),4,'GRU','MEX');
-INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),5,'GRU','MCO');
 
 CREATE SEQUENCE hotel_sequence START WITH 1 INCREMENT BY 1;
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),1,5);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),2,2);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),3,3);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),4,10);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),5,30);
 
