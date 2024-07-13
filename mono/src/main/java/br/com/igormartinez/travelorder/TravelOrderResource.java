ba9d805d38ca0bd5220007473886371073cdfe49
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-13 15:34:43 -0300

    feat(travel-order): Create real travel-order with findall, findbyid and create methods

---
+++ mono/src/main/java/br/com/igormartinez/travelorder/TravelOrderResource.java
@@ -0,0 +1,40 @@
+package br.com.igormartinez.travelorder;
+
+import java.util.List;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("travel-order")
+public class TravelOrderResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<TravelOrder> findAll() {
+        return TravelOrder.listAll();
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public TravelOrder findById(@PathParam("id") long id) {
+        return TravelOrder.findById(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public TravelOrder create(TravelOrder order) {
+        order.id = null;
+        order.persist();
+        
+        return order; 
+    }
+}
