58d7e6a790d3d639ee6b3c77193ae6e43a822f98
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-13 16:06:46 -0300

    feat(hotel): Create realm hotel with findall, findbyid and create methods

--- mono/src/main/resources/import.sql
+++ mono/src/main/resources/import.sql
@@ -11,3 +11,11 @@ INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('f
 INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),3,'GRU','ATL');
 INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),4,'GRU','MEX');
 INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),5,'GRU','MCO');
+
+CREATE SEQUENCE hotel_sequence START WITH 1 INCREMENT BY 1;
+INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),1,5);
+INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),2,2);
+INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),3,3);
+INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),4,10);
+INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),5,30);
+
