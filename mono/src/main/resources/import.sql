b08176c6b2cc256c71a1a04b8110a1404980be35
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-13 15:58:40 -0300

    feat(flight): Create realm flight with findall, findbyid and create methods

--- mono/src/main/resources/import.sql
+++ mono/src/main/resources/import.sql
@@ -4,3 +4,10 @@ INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
 INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
 INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
 INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
+
+CREATE SEQUENCE flight_sequence START WITH 1 INCREMENT BY 1;
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),1,'GRU','MCO');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),2,'GRU','JFK');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),3,'GRU','ATL');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),4,'GRU','MEX');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),5,'GRU','MCO');
