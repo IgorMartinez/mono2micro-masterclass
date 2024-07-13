daf1823766f4e210fc8c50238bd79e202f1b3ee7
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-13 16:07:19 -0300

    refactor(flight): Change to use plural in URL path

--- mono/src/main/java/br/com/igormartinez/flight/FlightResource.java
+++ mono/src/main/java/br/com/igormartinez/flight/FlightResource.java
@@ -11,7 +11,7 @@ import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("flight")
+@Path("flights")
 public class FlightResource {
     
     @GET
