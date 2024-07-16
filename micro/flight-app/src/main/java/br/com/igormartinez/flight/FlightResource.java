f492c5c5fe5b0e8684e0a6e3041f62f21162d5a5
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-15 21:20:12 -0300

    feat(micro-flight): Copy the project from mono to micro

---
+++ micro/flight-app/src/main/java/br/com/igormartinez/flight/FlightResource.java
@@ -0,0 +1,47 @@
+package br.com.igormartinez.flight;
+
+import java.util.List;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("flights")
+public class FlightResource {
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Flight> findAll() {
+        return Flight.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight findById(@PathParam("id") long id) {
+        return Flight.findById(id);
+    }
+
+    @GET
+    @Path("/travel-order/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight findByTravelOrderId(@PathParam("id") long id) {
+        return Flight.findByTravelOrderId(id);
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Flight create(Flight flight) {
+        flight.id = null;
+        flight.persist();
+
+        return flight;
+    }
+}
--- micro/flight-app/src/main/resources/import.sql
+++ micro/flight-app/src/main/resources/import.sql
@@ -1,6 +1,6 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+CREATE SEQUENCE flight_sequence START WITH 1 INCREMENT BY 1;
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),1,'GRU','MCO');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),2,'GRU','JFK');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),3,'GRU','ATL');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),4,'GRU','MEX');
+INSERT INTO Flight(id, travelorderid, fromAirport, toAirport) VALUES (NEXTVAL('flight_sequence'),5,'GRU','MCO');
