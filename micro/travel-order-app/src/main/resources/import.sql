7c7706a55e0932fd047607cc3a63ae3bde0e5c9c
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-17 20:24:56 -0300

    refactor(micro-travel-order): Remove startup script

--- micro/travel-order-app/src/main/resources/import.sql
+++ micro/travel-order-app/src/main/resources/import.sql
@@ -1,6 +1,1 @@
 CREATE SEQUENCE travelorder_sequence START WITH 1 INCREMENT BY 1;
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
-INSERT INTO TravelOrder(id) VALUES (NEXTVAL('travelorder_sequence'));
