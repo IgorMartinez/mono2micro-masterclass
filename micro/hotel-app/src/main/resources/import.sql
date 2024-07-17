67c39c7945d1d053e73f8f61582ab692e3ccc0fc
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-17 20:24:47 -0300

    refactor(micro-hotel): Remove startup script

--- micro/hotel-app/src/main/resources/import.sql
+++ micro/hotel-app/src/main/resources/import.sql
@@ -1,6 +1,1 @@
 CREATE SEQUENCE hotel_sequence START WITH 1 INCREMENT BY 1;
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),1,5);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),2,2);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),3,3);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),4,10);
-INSERT INTO Hotel(id, travelorderid, nights) VALUES (NEXTVAL('hotel_sequence'),5,30);
