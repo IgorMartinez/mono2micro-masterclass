e42b7b3efbd4fbf6bf547890eb596e903be5d416
Igor Martinez <igorgabriel.martinez@gmail.com>
2024-07-17 21:13:54 -0300

    feat(micro-travel-order): Add liveness, readiness and fault tolerance

--- micro/travel-order-app/src/main/java/br/com/igormartinez/travelorder/FlightService.java
+++ micro/travel-order-app/src/main/java/br/com/igormartinez/travelorder/FlightService.java
@@ -1,5 +1,10 @@
 package br.com.igormartinez.travelorder;
 
+import java.time.temporal.ChronoUnit;
+
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 import jakarta.ws.rs.Consumes;
@@ -21,10 +26,26 @@ public interface FlightService {
     @GET
     @Path("/travel-order/{id}")
     @Produces(MediaType.APPLICATION_JSON)
+    @Timeout(unit = ChronoUnit.SECONDS, value = 2)
+    @Fallback(fallbackMethod = "fallbackFindByTravelOrderId")
+    @CircuitBreaker(
+        requestVolumeThreshold = 4,
+        failureRatio = 0.5,
+        delay = 5000, 
+        successThreshold = 2 
+    )
     public Flight findByTravelOrderId(@PathParam("id") long id);
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Flight create(Flight flight);
+
+    default Flight fallbackFindByTravelOrderId(@PathParam("id") long id) {
+        Flight flight = new Flight();
+        flight.setTravelOrderId(id);
+        flight.setFromAirport("");
+        flight.setToAirport("");
+        return flight;
+    }
 }
---
+++ micro/travel-order-app/src/main/java/br/com/igormartinez/travelorder/ReadinessCheck.java
@@ -0,0 +1,30 @@
+package br.com.igormartinez.travelorder;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import jakarta.inject.Inject;
+
+@Readiness
+public class ReadinessCheck implements HealthCheck {
+
+    @Inject
+    @RestClient
+    FlightService flightService;
+
+    @Inject
+    @RestClient
+    HotelService hotelService;
+
+    @Override
+    public HealthCheckResponse call() {
+        if (flightService.findById(1) != null && hotelService.findById(1) != null) {
+            return HealthCheckResponse.up("estou pronto");
+        } else {
+            return HealthCheckResponse.down("não estou pronto");
+        }
+    }
+    
+}
